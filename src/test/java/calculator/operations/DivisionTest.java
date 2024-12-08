3fe4c081b397612d300c7b5b6b603d3e82aecb3a
Dave Tenn <davy.lsdev@gmail.com>
2024-12-07 22:35:33 -0700

    Teach calculator to divide

---
+++ src/test/java/calculator/operations/DivisionTest.java
@@ -0,0 +1,39 @@
+package calculator.operations;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import calculator.Calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DivisionTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testBasicDivision() {
+        assertEquals(2, calculator.divide(4, 2), "4 / 2 should equal 2");
+        assertEquals(2, calculator.divide(10, 5), "10 / 5 should equal 2");
+        assertEquals(1, calculator.divide(3, 3), "3 / 3 should equal 1");
+    }
+
+    @Test
+    void testDivisionWithZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0), "5 / 0 should throw an exception");
+        assertEquals(0, calculator.divide(0, 3),"0 / 3 should should equal 0");
+        assertThrows(ArithmeticException.class, () -> calculator.divide(0, 0), "0 / 0 should throw an exception");
+    }
+
+    @Test
+    void testDivisionWithNegativeNumbers() {
+        assertEquals(1, calculator.divide(-3, -2), "-3 / -2 should equal 1");
+        assertEquals(-1, calculator.divide(-4, 3), "-4 / 3 should equal -1");
+        assertEquals(-1, calculator.divide(4, -3), "4 / -3 should equal -1");
+    }
+
+} 
